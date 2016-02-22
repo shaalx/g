2994a5758bb81bf2c30a81dd78abc6954b768205
shaalx <shaalx@yahoo.com.sg>
2016-02-22 11:45:24 +0800

    auto

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/everfore/exc"
+	// "github.com/shaalx/goutils"
+)
+
+var (
+	gb = false
+	gt = false
+	gr = ""
+	gi = false
+	gs = false
+)
+
+func init() {
+	flag.BoolVar(&gb, "b", false, "-b [true] : go build")
+	flag.BoolVar(&gi, "i", false, "-i [true] : go install")
+	flag.BoolVar(&gt, "t", false, "-t [true] : go test -v")
+	flag.StringVar(&gr, "r", "main.go", "-r main.go : go run file.go")
+
+	flag.BoolVar(&gs, "s", false, "-s [true] : git status")
+}
+
+func main() {
+	flag.Parse()
+	cmd := exc.NewCMD("git status").Debug()
+	if gs {
+		cmd.Execute()
+		return
+	}
+	if gb {
+		cmd.Reset("go build").Execute()
+		return
+	}
+	if gi {
+		cmd.Reset("go install").Execute()
+		return
+	}
+	if gt {
+		cmd.Reset("go test -v").Execute()
+		return
+	}
+	cmd.Reset(fmt.Sprintf("go run %s", gr)).Execute()
+	return
+}
